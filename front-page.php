d9349559d5c6962624a9bd0094d28cc8a09ccbc7
Andrew Minion <andrew@andrewrminion.com>
2015-11-13 17:14:17 -0500

    add testimonial content to homepage template

--- front-page.php
+++ front-page.php
@@ -33,4 +33,44 @@ add_filter( 'genesis_post_meta', function() { return false; } );
 // remove pagination
 remove_action( 'genesis_after_endwhile', 'genesis_posts_nav' );
 
+// add loop for testimonials
+add_action( 'genesis_before_footer', 'ora_testimonial_loop', 5 );
+function ora_testimonial_loop() {
+    // WP_Query arguments
+    $args = array (
+        'post_type'              => array( 'testimonial' ),
+        'pagination'             => false,
+        'posts_per_page'         => '4',
+    );
+
+    // The Query
+    $testimonial_query = new WP_Query( $args );
+
+    // The Loop
+    if ( $testimonial_query->have_posts() ) {
+        echo '<section class="testimonials">
+        <section class="testimonials-inner wrap">';
+
+        while ( $testimonial_query->have_posts() ) {
+            $testimonial_query->the_post();
+
+            echo '<article class="testimonial single">';
+            // post thumbnail
+            if ( has_post_thumbnail() ) {
+                the_post_thumbnail( array( 80, 80 ), array( 'class' => 'testimonial-thumb' ) );
+            }
+
+            // content
+            echo '<div class="testimonial-content-wrapper"><p class="testimonial-content">' . wptexturize( get_the_content() ) . '</p><p class="testimonial-title alternate">' . get_the_title() . ', ' . get_post_meta( get_the_ID(), 'personal_info_location', true ) . '</div>';
+
+            echo '</article>';
+        }
+        echo '</section>
+        </section>';
+    }
+
+    // Restore original Post Data
+    wp_reset_postdata();
+}
+
 genesis();
