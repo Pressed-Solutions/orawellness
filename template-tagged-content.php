d6ca925deda35454dcece29a138b5408147d14d4
Andrew Minion <andrew@andrewrminion.com>
2015-11-19 18:24:57 -0500

    add tagged posts to page template

--- template-tagged-content.php
+++ template-tagged-content.php
@@ -8,7 +8,7 @@
 add_filter( 'genesis_post_title_text', 'ora_tagged_content_subheading' );
 function ora_tagged_content_subheading( $title ) {
     if ( get_field( 'page_subtitle' ) ) {
-        $title .= '<h2 class="entry-subtitle alternate">' . get_field( 'page_subtitle' ) . '</h2>';
+        $title .= '<h2 class="entry-subtitle">' . get_field( 'page_subtitle' ) . '</h2>';
     }
 
     return $title;
@@ -21,5 +21,39 @@ function ora_show_thumbnail() {
         the_post_thumbnail( 'tagged-content', array( 'class' => 'alignright' ) );
     }
 }
+
+// Add loop for tagged posts
+add_action( 'genesis_after_loop', 'ora_tagged_posts_loop', 7 );
+function ora_tagged_posts_loop() {
+    $post_tag = esc_attr( get_field( 'post_tag' )->slug );
+
+    // WP_Query arguments
+    $tagged_posts_args = array (
+        'post_type'              => array( 'post' ),
+        'tag'                    => $post_tag,
+        'pagination'             => true,
+        'posts_per_page'         => '3',
+    );
+
+    // The Query
+    $tagged_posts_query = new WP_Query( $tagged_posts_args );
+
+    // The Loop
+    if ( $tagged_posts_query->have_posts() ) {
+
+        // output content
+        echo '<section class="tagged-content">
+        <h2 class="alternate">Recent Posts <em>about</em> ' . esc_attr( get_field( 'post_tag' )->name ) . '</h2>';
+        while ( $tagged_posts_query->have_posts() ) {
+            $tagged_posts_query->the_post();
+            echo '<article class="tagged-content-article"><h3 class="title"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h3><a href="' . get_permalink() . '">' . get_the_post_thumbnail( get_the_ID(), array( 160, 160 ), array( 'class' => 'alignright' ) ) . '</a>' . get_the_excerpt() . '</article>';
+        }
+        echo '</section>';
+    }
+
+    // Restore original Post Data
+    wp_reset_postdata();
+}
+
 
 genesis();
