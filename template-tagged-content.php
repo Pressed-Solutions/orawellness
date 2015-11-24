f0d802e8baea87fe510b7491ebc0fa8ce5de1847
Andrew Minion <andrew@andrewrminion.com>
2015-11-24 14:58:51 -0500

    check for post_tag before outputting any content

--- template-tagged-content.php
+++ template-tagged-content.php
@@ -25,43 +25,45 @@ function ora_show_thumbnail() {
 // Add loop for tagged posts
 add_action( 'genesis_after_loop', 'ora_tagged_posts_loop', 7 );
 function ora_tagged_posts_loop() {
-    $post_tag = esc_attr( get_field( 'post_tag' )->slug );
-    if ( get_query_var( 'paged' ) ) {
-        $paged = get_query_var( 'paged' );
-    } else {
-        $paged = 1;
-    }
-
-    // WP_Query arguments
-    $tagged_posts_args = array (
-        'post_type'              => array( 'post' ),
-        'tag'                    => $post_tag,
-        'pagination'             => true,
-        'posts_per_page'         => '3',
-        'paged'                  => $paged,
-    );
-
-    // The Query
-    $tagged_posts_query = new WP_Query( $tagged_posts_args );
-
-    // The Loop
-    if ( $tagged_posts_query->have_posts() ) {
-
-        // output content
-        echo '<section class="tagged-content">
-        <h2 class="alternate">Recent Posts <em>about</em> ' . esc_attr( get_field( 'post_tag' )->name ) . '</h2>';
-
-        while ( $tagged_posts_query->have_posts() ) {
-            $tagged_posts_query->the_post();
-            echo '<article class="tagged-content-article"><h3 class="title"><a href="' . get_permalink() . '">' . get_the_post_thumbnail( get_the_ID(), array( 160, 160 ), array( 'class' => 'alignright' ) ) . get_the_title() . '</a></h3>' . get_the_excerpt() . '</article>';
+    if ( get_field( 'post_tag' ) ) {
+        $post_tag = esc_attr( get_field( 'post_tag' )->slug );
+        if ( get_query_var( 'paged' ) ) {
+            $paged = get_query_var( 'paged' );
+        } else {
+            $paged = 1;
         }
 
-        custom_pagination( $tagged_posts_query->max_num_pages, "", $paged );
-        echo '</section>';
-    }
+        // WP_Query arguments
+        $tagged_posts_args = array (
+            'post_type'              => array( 'post' ),
+            'tag'                    => $post_tag,
+            'pagination'             => true,
+            'posts_per_page'         => '3',
+            'paged'                  => $paged,
+        );
 
-    // Restore original Post Data
-    wp_reset_postdata();
+        // The Query
+        $tagged_posts_query = new WP_Query( $tagged_posts_args );
+
+        // The Loop
+        if ( $tagged_posts_query->have_posts() ) {
+
+            // output content
+            echo '<section class="tagged-content">
+            <h2 class="alternate">Recent Posts <em>about</em> ' . esc_attr( get_field( 'post_tag' )->name ) . '</h2>';
+
+            while ( $tagged_posts_query->have_posts() ) {
+                $tagged_posts_query->the_post();
+                echo '<article class="tagged-content-article"><h3 class="title"><a href="' . get_permalink() . '">' . get_the_post_thumbnail( get_the_ID(), array( 160, 160 ), array( 'class' => 'alignright' ) ) . get_the_title() . '</a></h3>' . get_the_excerpt() . '</article>';
+            }
+
+            custom_pagination( $tagged_posts_query->max_num_pages, "", $paged );
+            echo '</section>';
+        }
+
+        // Restore original Post Data
+        wp_reset_postdata();
+    }
 }
 
 
