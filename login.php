9cf810a9af441729b30059587a488f4e5a30e368
Andrew Minion <andrew@andrewrminion.com>
2015-12-24 16:12:06 -0500

    remove header and sidebar stuff if user is not logged in

--- login.php
+++ login.php
@@ -3,44 +3,14 @@
  * Template Name: Login/Logout Page
  */
 
-//* Remove default page content
-remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
-
-//* Add custom login form to page content
-function ora_do_login_form() {
-    $loggedin = is_user_logged_in();
-    $user = wp_get_current_user();
-    if ( $loggedin ) { ?>
-
-        <h3>You are already logged in!</h3>
-        <p>Hello, <?php echo $user->user_firstname; ?>! Looks like you are already signed in. Thanks for being a part of this website!</p>
-        <p><a href="/">Go to Homepage</a> or <a href="<?php echo wp_logout_url( get_permalink() ); ?>">Log Out</a></p>
-
-    <?php
-    } else {
-        $args = array(
-            'form_id'			=> 'loginform',
-            'redirect'			=> get_bloginfo( 'url' ),
-            'id_username'		=> 'user_login',
-            'id_password'		=> 'user_pass',
-            'id_remember'		=> 'rememberme',
-            'id_submit'			=> 'wp-submit',
-            'label_username'	=> __( 'Username' ),
-            'label_password'	=> __( 'Password' ),
-            'label_remember'	=> __( 'Remember Me' ),
-            'label_log_in'		=> __( 'Log In' ),
-        );
-        wp_login_form( $args );
-    }
+//* If not logged in, remove all menus, header, sidebar, footer
+if ( ! is_user_logged_in() ) {
+    remove_action( 'genesis_header', 'genesis_do_header' );
+    remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+    remove_action( 'genesis_before_header', 'ora_add_tertiary_menu' );
+    remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
+    remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+    remove_theme_support( 'genesis-footer-widgets', 3 );
 }
-add_action( 'genesis_entry_content', 'ora_do_login_form' );
-
-//* Remove all menus, header, sidebar, footer
-remove_action( 'genesis_header', 'genesis_do_header' );
-remove_action( 'genesis_after_header', 'genesis_do_subnav' );
-remove_action( 'genesis_before_header', 'ora_add_tertiary_menu' );
-remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
-remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
-remove_theme_support( 'genesis-footer-widgets', 3 );
 
 genesis();
