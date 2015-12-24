11e7b7b31a53dfe59a9b403ae1d53ac02a5b65b9
Andrew Minion <andrew@andrewrminion.com>
2015-12-24 11:16:54 -0500

    remove header, footer, sidebar, and menus from login template

--- login.php
+++ login.php
@@ -35,4 +35,12 @@ function ora_do_login_form() {
 }
 add_action( 'genesis_entry_content', 'ora_do_login_form' );
 
+//* Remove all menus, header, sidebar, footer
+remove_action( 'genesis_header', 'genesis_do_header' );
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+remove_action( 'genesis_before_header', 'ora_add_tertiary_menu' );
+remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+remove_theme_support( 'genesis-footer-widgets', 3 );
+
 genesis();
