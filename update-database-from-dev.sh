6f19ff76fe3005a6e7f20d08f4fcaef731df18da
Andrew Minion <andrew@andrewrminion.com>
2015-12-30 15:08:21 -0500

    add script to update database from dev

---
+++ update-database-from-dev.sh
@@ -0,0 +1,6 @@
+if [ ! -f "database-latest.sql.xz" ]; then
+    ssh PressedSolutions 'cd /home/orawellness/ && DATE=$(date +"%Y-%m-%d-%H%M") && mysqldump --defaults-file=/home/orawellness/.mysqldump.conf orawelln_wp | xz > /home/orawellness/database-backups/database-$DATE.sql.xz && ln -f -s /home/orawellness/database-backups/database-$DATE.sql.xz /home/orawellness/database-latest.sql.xz' && scp PressedSolutions:/home/orawellness/database-latest.sql.xz ./
+fi
+
+# import
+xzcat -c database-latest.sql.xz | mysql --defaults-file=~/.mysql_credentials orawelln_wp && mysql --defaults-file=~/.mysql_credentials --execute="USE orawelln_wp; UPDATE wpora_options SET option_value = 'http://orawellness.dev' WHERE option_value = 'http://orawellness.pressedsolutions.com';" && mv database-latest.sql.xz ~/.Trash/
