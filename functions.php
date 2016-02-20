8df0ffe8cd9a7672b6be61df76b7c25c2d7e7896
Andrew Minion <andrew@andrewrminion.com>
2016-02-20 16:54:10 -0500

    add custom favicons

--- functions.php
+++ functions.php
@@ -512,3 +512,29 @@ function ora_show_custom_widget() {
     <?php }
 }
 add_action( 'genesis_before_sidebar_widget_area', 'ora_show_custom_widget' );
+
+//* Add favicons
+add_filter( 'genesis_pre_load_favicon', function() { return NULL; } );
+add_action( 'wp_head', 'ora_custom_favicons' );
+function ora_custom_favicons() {
+    echo '    <link rel="apple-touch-icon" sizes="57x57" href="' . get_stylesheet_directory_uri() . '/images/favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="' . get_stylesheet_directory_uri() . '/images/favicons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="' . get_stylesheet_directory_uri() . '/images/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="' . get_stylesheet_directory_uri() . '/images/favicons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="' . get_stylesheet_directory_uri() . '/images/favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="' . get_stylesheet_directory_uri() . '/images/favicons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="' . get_stylesheet_directory_uri() . '/images/favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="' . get_stylesheet_directory_uri() . '/images/favicons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="' . get_stylesheet_directory_uri() . '/images/favicons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/images/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/images/favicons/favicon-194x194.png" sizes="194x194">
+    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/images/favicons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/images/favicons/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/images/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="' . get_stylesheet_directory_uri() . '/images/favicons/manifest.json">
+    <link rel="mask-icon" href="' . get_stylesheet_directory_uri() . '/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <meta name="theme-color" content="#6ba26f">
+    ';
+}
