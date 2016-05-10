0e978703af3a4fec28f720c9409518714382bc80
Andrew Minion <andrew@andrewrminion.com>
2016-05-09 21:46:14 -0400

    add summit interviews templates and styles

---
+++ page-summit-interviews.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Description: The page used for the summit interviews archive
+ */
+
+// add loop for testimonials
+add_action( 'genesis_after_entry', 'ora_summit_interview_loop', 5 );
+function ora_summit_interview_loop() {
+    if ( get_query_var( 'paged' ) ) {
+        $paged = get_query_var( 'paged' );
+    } else {
+        $paged = 1;
+    }
+
+    // WP_Query arguments
+    $args = array (
+        'post_type'              => array( 'summit_interview' ),
+        'posts_per_page'         => '5',
+        'pagination'             => true,
+        'paged'                  => $paged,
+    );
+
+    // The Query
+    $summit_interview_query = new WP_Query( $args );
+
+    // Search form
+    echo '<form method="get" action="' . get_option( 'home' ) . '/" class="search-form" >
+        <input type="search" value="'. $search_text .'" name="s" placeholder="Search summit interviews" />
+        <input type="hidden" name="post_type" value="summit_interview" />
+        <input type="submit" value="Search summit interviews" />
+    </form>
+    ';
+
+    // The Loop
+    if ( $summit_interview_query->have_posts() ) {
+        echo '<section class="custom-archive expert-interview">';
+        while ( $summit_interview_query->have_posts() ) {
+            $summit_interview_query->the_post();
+
+            echo '<article class="expert-interview single clearfix">';
+                // post thumbnail
+                if ( get_field( 'photo' ) ) {
+                    echo '<a href="' . get_permalink() . '">' . wp_get_attachment_image( get_field( 'photo' ), array( 80, 80 ), true, array( 'class' => 'alignright avatar' ) ) . '</a>';
+                }
+
+                // title
+                echo '<h2><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
+                // content
+                echo '<p class="expert-interview-content">' . get_the_excerpt() . '</p>';
+            echo '</article>';
+        }
+        custom_pagination( $summit_interview_query->max_num_pages, "", $paged );
+        echo '</section><!-- .custom-archive.expert-interview -->';
+    }
+
+    // Restore original Post Data
+    wp_reset_postdata();
+}
+
+// Add page subtitle and banner
+add_filter( 'genesis_post_title_text', 'ora_tagged_content_subheading' );
+function ora_tagged_content_subheading( $title ) {
+    if ( get_field( 'page_subtitle' ) ) {
+        $title = '<h1 class="entry-title">' . get_field( 'page_subtitle' ) . '</h1>';
+    }
+
+    if ( get_field( 'header_image' ) ) {
+        $title .= '<style>.entry-header { background-image: url(\'' . get_field( 'header_image' ) . '\'); } </style>';
+    }
+
+    return $title;
+}
+
+// Add membership sidebar
+add_action( 'genesis_after_content', 'ora_show_membership_sidebar' );
+function ora_show_membership_sidebar() {
+    echo '<aside class="sidebar sidebar-membership widget-area" role="complementary" aria-label="Membership Pages Sidebar" itemscope itemtype="http://schema.org/WPSideBar" id="genesis-sidebar-membership-pages">';
+    dynamic_sidebar( 'membership' );
+    echo '</aside>';
+}
+
+// Remove default sidebars
+remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
+unregister_sidebar( 'sidebar-alt' );
+unregister_sidebar( 'sidebar-primary' );
+
+genesis();
---
+++ single-summit_interview.php
@@ -0,0 +1,62 @@
+<?php
+/**
+* Description: Expert Interviews
+*/
+
+// Add expert interview content
+add_action( 'genesis_entry_content', 'ora_expert_interviews_content', 8 );
+function ora_expert_interviews_content() {
+    // author info
+    echo '<section class="author-info">';
+        echo wp_get_attachment_image( get_field( 'photo' ), array( 80, 80 ), true, array( 'class' => 'avatar' ) );
+        if ( get_field( 'expert_title' ) ) {
+            echo '<h3 class="alternate">' . get_field( 'expert_title' ) . '</h3>';
+        }
+        echo '<h2>' . get_field( 'expert_name' ) . '</h2>';
+        if ( get_field( 'biography' ) ) {
+            echo wpautop( get_field( 'biography' ) );
+        }
+    echo '</section><!-- .author-info-->';
+
+    // introduction
+    the_field( 'introduction' );
+
+    // episode summary
+    if ( get_field( 'episode_summary' ) ) {
+        echo '<p>In this episode, we cover:</p>';
+        the_field( 'episode_summary' );
+    }
+
+    // links discussed
+    if ( get_field( 'links_discussed' ) ) {
+        echo '<h2>Links Discussed</h2>';
+        the_field( 'links_discussed' );
+    }
+
+    // media
+    echo '<h2>Listen</h2>';
+    echo wp_oembed_get( esc_url( get_field( 'media_url' ) ) );
+
+    // transcript
+    echo '<h2>Transcript</h2>';
+
+    // transcript PDF
+    if ( get_field( 'transcript_pdf_link' ) ) {
+        echo '<a class="transcript-link alignright" href=' . esc_url( get_field( 'transcript_pdf_link' ) ) . '" target="_blank">Download Transcript PDF</a>';
+    }
+}
+
+// Add membership sidebar
+add_action( 'genesis_after_content', 'ora_show_membership_sidebar' );
+function ora_show_membership_sidebar() {
+    echo '<aside class="sidebar sidebar-membership widget-area" role="complementary" aria-label="Membership Pages Sidebar" itemscope itemtype="http://schema.org/WPSideBar" id="genesis-sidebar-membership-pages">';
+    dynamic_sidebar( 'membership' );
+    echo '</aside>';
+}
+
+// Remove default sidebars
+remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
+unregister_sidebar( 'sidebar-alt' );
+unregister_sidebar( 'sidebar-primary' );
+
+genesis();
