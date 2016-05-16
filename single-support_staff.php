c7ebf30e054a7c273296208dbddfa6fee52bf035
Andrew Minion <andrew@andrewrminion.com>
2016-05-16 15:25:16 -0400

    add link on single support staff for all support staff

--- single-support_staff.php
+++ single-support_staff.php
@@ -14,4 +14,11 @@ function ora_show_thumbnail() {
 // Remove post meta
 add_filter( 'genesis_post_info', function() { return false; } );
 
+// Add link to all staff
+add_filter( 'the_content', 'ora_add_support_staff_link', 5 );
+function ora_add_support_staff_link( $content ) {
+    $support_staff_obj = get_post_type_object( 'support_staff' );
+    return $content . '<p><a href="' . home_url( '/' . $support_staff_obj->rewrite['slug'] . '/' ) . '">Meet the rest of our team</a></p>';
+}
+
 genesis();
