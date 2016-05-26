767974f459b3c80a06ee63d38ff418771798b233
Andrew Minion <andrew@andrewrminion.com>
2016-05-26 10:49:08 -0400

    add border-radius to all testimonial images

--- functions.php
+++ functions.php
@@ -681,3 +681,14 @@ function wcs_woo_remove_reviews_tab($tabs) {
 
 //* Enable “create an account” on purchase by default
 add_filter( 'woocommerce_create_account_default_checked', '__return_true' );
+
+//* Add .testimonial-thumb to all testimonial images
+add_filter('wp_get_attachment_image_attributes', 'ora_testimonial_image_class_filter');
+function ora_testimonial_image_class_filter( $attr ) {
+    global $post;
+    if ( 'testimonial' == $post->post_type ) {
+        $attr['class'] .= ' testimonial-thumb';
+    }
+
+    return $attr;
+}
