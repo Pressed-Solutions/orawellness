447434a5e824b3fc06bec99cc6a3cd413ba34ec7
Andrew Minion <andrew@andrewrminion.com>
2016-06-16 11:56:05 -0400

    update WooCommerce templates

--- woocommerce/myaccount/form-reset-password.php
+++ woocommerce/myaccount/form-reset-password.php
@@ -26,11 +26,11 @@ wc_print_notices(); ?>
 
     <p><?php echo apply_filters( 'woocommerce_reset_password_message', __( 'Enter a new password below.', 'woocommerce') ); ?></p>
 
-    <p class="woocommerce-FormRow woocommerce-FormRow--first form-row form-row-first">
+    <p class="woocommerce-FormRow woocommerce-FormRow--first form-row form-row-first form-row-wide">
         <label for="password_1"><?php _e( 'New password', 'woocommerce' ); ?> <span class="required">*</span></label>
         <input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="password_1" id="password_1" />
     </p>
-    <p class="woocommerce-FormRow woocommerce-FormRow--last form-row form-row-last">
+    <p class="woocommerce-FormRow woocommerce-FormRow--last form-row form-row-last form-row-wide">
         <label for="password_2"><?php _e( 'Re-enter new password', 'woocommerce' ); ?> <span class="required">*</span></label>
         <input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="password_2" id="password_2" />
     </p>
