9169cb92e60718df292e3c803cc189c0eecd28d9
Andrew Minion <andrew@andrewrminion.com>
2016-07-06 10:16:29 -0400

    use default WP login action and field names
    Fixes issue with Memberium and newly-created users

--- woocommerce/myaccount/form-login.php
+++ woocommerce/myaccount/form-login.php
@@ -37,18 +37,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php endif; ?>
 
-        <form method="post" class="login">
+        <form method="post" class="login" action="<?php echo wp_login_url(); ?>">
             <h2>Login</h2>
 
             <?php do_action( 'woocommerce_login_form_start' ); ?>
 
             <p class="woocommerce-FormRow woocommerce-FormRow--wide form-row form-row-wide">
-                <label for="username"><?php _e( 'Username or email address', 'woocommerce' ); ?> <span class="required">*</span></label>
-                <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="username" value="<?php if ( ! empty( $_POST['username'] ) ) echo esc_attr( $_POST['username'] ); ?>" />
+                <label for="log"><?php _e( 'Username or email address', 'woocommerce' ); ?> <span class="required">*</span></label>
+                <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="log" id="username" value="<?php if ( ! empty( $_POST['username'] ) ) echo esc_attr( $_POST['username'] ); ?>" />
             </p>
             <p class="woocommerce-FormRow woocommerce-FormRow--wide form-row form-row-wide">
-                <label for="password"><?php _e( 'Password', 'woocommerce' ); ?> <span class="required">*</span></label>
-                <input class="woocommerce-Input woocommerce-Input--text input-text" type="password" name="password" id="password" />
+                <label for="pwd"><?php _e( 'Password', 'woocommerce' ); ?> <span class="required">*</span></label>
+                <input class="woocommerce-Input woocommerce-Input--text input-text" type="password" name="pwd" id="password" />
             </p>
 
             <?php do_action( 'woocommerce_login_form' ); ?>
