b7bb10eed45f5cbc7ddd9ac68d922e87a99eaa8f
Andrew Minion <andrew@andrewrminion.com>
2016-07-08 17:29:48 -0400

    add form for updating email address and fix styling

--- woocommerce/myaccount/form-edit-account.php
+++ woocommerce/myaccount/form-edit-account.php
@@ -22,37 +22,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 do_action( 'woocommerce_before_edit_account_form' ); ?>
 
-<form class="woocommerce-EditAccountForm edit-account" action="" method="post">
-
-    <?php do_action( 'woocommerce_edit_account_form_start' ); ?>
-
-    <p class="woocommerce-FormRow woocommerce-FormRow--first form-row form-row-first">
-        <label for="account_first_name"><?php _e( 'First name', 'woocommerce' ); ?> <span class="required">*</span></label>
-        <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_first_name" id="account_first_name" value="<?php echo esc_attr( $user->first_name ); ?>" />
-    </p>
-    <p class="woocommerce-FormRow woocommerce-FormRow--last form-row form-row-last">
-        <label for="account_last_name"><?php _e( 'Last name', 'woocommerce' ); ?> <span class="required">*</span></label>
-        <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_last_name" id="account_last_name" value="<?php echo esc_attr( $user->last_name ); ?>" />
-    </p>
-    <div class="clear"></div>
-
-    <p class="woocommerce-FormRow woocommerce-FormRow--wide form-row form-row-wide">
-        <label for="account_email"><?php _e( 'Email address', 'woocommerce' ); ?> <span class="required">*</span></label>
-        <input type="email" class="woocommerce-Input woocommerce-Input--email input-text" name="account_email" id="account_email" value="<?php echo esc_attr( $user->user_email ); ?>" />
-    </p>
-
-    <div class="clear"></div>
-
-    <?php do_action( 'woocommerce_edit_account_form' ); ?>
-
-    <p>
-        <?php wp_nonce_field( 'save_account_details' ); ?>
-        <input type="submit" class="woocommerce-Button button" name="save_account_details" value="<?php esc_attr_e( 'Save changes', 'woocommerce' ); ?>" />
-        <input type="hidden" name="action" value="save_account_details" />
-    </p>
-
-    <?php do_action( 'woocommerce_edit_account_form_end' ); ?>
-</form>
+<h2>Change Email Address/Username</h2>
+<?php echo do_shortcode( '[memb_change_email]' ); ?>
 
 <h2>Change Password</h2>
 <?php echo do_shortcode( '[memb_change_password]' ); ?>
