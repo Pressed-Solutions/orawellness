2a536ba953ff4bba70fe1b2bcd26e873e4e4546e
Andrew Minion <andrew@andrewrminion.com>
2016-07-20 16:57:18 -0400

    disable hidden CC fields

--- js/saved-card-CVV.js
+++ js/saved-card-CVV.js
@@ -4,7 +4,8 @@
     $( 'body' ).on( 'change', 'input.woocommerce-SavedPaymentMethods-tokenInput', function() {
         if ( $( this ).val() !== 'new' ) {
             $( target ).show();
-            $( target + ' input[name="infusionsoft-card-cvc"]' ).attr( 'required', 'required' );
+            $( target + ' input[name="infusionsoft-card-cvc"]' ).attr( 'required', true );
+            $( '#wc-infusionsoft-cc-form input' ).attr( 'disabled', true );
         } else {
             $( target ).hide();
             $( target + ' input[name="infusionsoft-card-cvc"]' ).attr( 'required' , null );
