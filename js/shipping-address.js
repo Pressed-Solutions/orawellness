3097fb0fedb69327f70759d75ccf1836260139e4
Andrew Minion <andrew@andrewrminion.com>
2016-11-18 14:41:48 -0500

    update documentation

--- js/shipping-address.js
+++ js/shipping-address.js
@@ -2,6 +2,7 @@
     $(document).ready(function(){
         $('.woocommerce-shipping-fields').on('change', 'input#shipping-billing-addresses-checkbox', function(){
             if ($(this).attr('checked') == 'checked') {
+                // if it’s checked, copy over billing info
                 $('.shipping_address input:not([class*="select2"]), .shipping_address select').each(function(){
                     var shippingName = $(this).attr('name'),
                         billingName = shippingName.replace('shipping', 'billing'),
@@ -10,6 +11,7 @@
                     $(this).val(billingValue).trigger('change');
                 });
             } else {
+                // otherwise trigger change on the select2 elements to update them
                 $('.shipping_address input:not([class*="select2"]), .shipping_address select').val(null).trigger('change');
             }
         });
