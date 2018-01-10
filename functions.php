f5300926a6d1586e87bdac8196ee2c5457e198a2
Andrew Minion <andrew@andrewrminion.com>
2018-01-10 16:30:41 -0500

    remove old scripts

--- functions.php
+++ functions.php
@@ -34,15 +34,9 @@ function ora_theme_JS() {
     // webfont loader
     wp_enqueue_script( 'webfonts', get_stylesheet_directory_uri() . '/js/webfonts.min.js', array(), filemtime( get_stylesheet_directory() . '/js/webfonts.min.js' ) );
 
-    // membership
-    wp_enqueue_script( 'membership', get_stylesheet_directory_uri() . '/js/membership.min.js', array( 'jquery' ), filemtime( get_stylesheet_directory() . '/js/membership.min.js' ) );
-
     // Add Brad Frost-inspired mobile menu JS
     wp_enqueue_script( 'mobile-menu-js', get_stylesheet_directory_uri() . '/js/navigation.min.js', array( 'jquery' ), filemtime( get_stylesheet_directory() . '/js/navigation.min.js' ) );
     wp_enqueue_script( 'modernizr', get_stylesheet_directory_uri() . '/js/modernizr-flexbox.min.js', array(), filemtime( get_stylesheet_directory() . '/js/modernizr-flexbox.min.js' ) );
-
-    // checkout
-    wp_register_script( 'ora-shipping-address', get_stylesheet_directory_uri() . '/js/shipping-address.min.js', array( 'jquery' ), filemtime( get_stylesheet_directory() . '/js/shipping-address.min.js' ) );
 }
 
 //* Add Hotjar script
